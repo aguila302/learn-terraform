1074e75b2fb30b46c1b57dbcc6525b3062d14dc1
Alfonso <alfonso@garagecoders.net>
2020-06-15 09:21:12 -0500

    config gcp

--- gcp.tf
+++ gcp.tf
@@ -1,7 +1,30 @@
-
-
 provider "google" {
   project = "learn-terraform-280319"
   region  = "us-west1"
   zone    = "us-west1-b"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  # auto_create_subnetworks = "true"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    # network       = "default"
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
 }
